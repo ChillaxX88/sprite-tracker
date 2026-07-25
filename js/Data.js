b512bfd1411ba4cf48fb06282bda54f8415f570d
ChillaxX88 <cjthestrong88@gmail.com>
2026-07-25 12:36:00 -0700

    Create Data.js

---
+++ js/Data.js
@@ -0,0 +1,20 @@
+const sprites = [
+    {
+        id: 1,
+        name: "Aura Sprite",
+        rarity: "Epic",
+        collected: false
+    },
+    {
+        id: 2,
+        name: "Ember Sprite",
+        rarity: "Rare",
+        collected: false
+    },
+    {
+        id: 3,
+        name: "Frost Sprite",
+        rarity: "Common",
+        collected: false
+    }
+];
